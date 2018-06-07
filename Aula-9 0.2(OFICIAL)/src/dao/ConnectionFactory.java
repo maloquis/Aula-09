a7dc77b455634086b0a8bb72c02ed9efce4b3e73
maloquis <rafaelgamer.trol@gmail.com>
2018-06-07 14:51:44 -0300

    Rafael Silva

---
+++ Aula-9 0.2(OFICIAL)/src/dao/ConnectionFactory.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+/**
+ * @author Rafael Silva
+ *
+ */
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+	static {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	// Obtém conexão com o banco de dados
+	public static Connection obtemConexao() throws SQLException {
+		return DriverManager
+				.getConnection("jdbc:mysql://localhost/Pais?user=Alunos&password=alunos");
+	}
+
+}
